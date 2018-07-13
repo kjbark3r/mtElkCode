3c4497a19bc3ec2142f5ea550f24e8cb33b41ab3
MCWRU01-LW17\kristin <kristin.barker@umconnect.umt.edu>
2018-07-13 11:25:35 -0600

    deleted old (temporary) code to remove first location of rNSD problem individuals

--- troubleshootingNSD.R
+++ troubleshootingNSD.R
@@ -36,25 +36,3 @@
         beep("complete")
       
         
-      ## Fix the problem for those individuals
-
-        # when i ran this, the issue was indivs who had a big gap between
-        # the date of their first loc and all subsequent locs -
-        # so below code removes the first loc for each indiv who had an error
-        
-        # identify the rows you want to remove (1st loc for each indiv)
-        rm <- locsModel %>%
-          filter(elkYr %in% as.character(errorsRloc$elkYr)) %>%
-          group_by(elkYr) %>%
-          filter(DT == min(DT)) %>%
-          ungroup()
-        
-        # remove those rows from the full dataframe (does nothing if no errors)
-        locsModel <- locsModel %>%
-          anti_join(rm)
-
-        # rerun "Identify individuals" section above to verify no more errors, then proceed
-        # note if you still get errors and have to do more work,
-        # you may need to recreate locsModel from the original code 
-        # (in case what you just did changed it in an undesirable way)
-      
